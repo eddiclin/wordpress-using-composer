04e1a5a32a2469d1f2fb3fbbb940367ba8b6a922
eddiclin <eddiclin@gmail.com>
2016-06-26 17:47:02 +0800

    first commit

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Register The Composer Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require('./wp/wp-blog-header.php');
---
+++ public/wp-config.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+/**
+ * Load MySQL settings and environment parameters.
+ */
+if (file_exists(dirname(__FILE__) . '/../production-config.php')) {
+    include(dirname(__FILE__) . '/../production-config.php');
+} else {
+    include(dirname(__FILE__) . '/../local-config.php');
+}
+
+// Important: Custom wp-content directory
+define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
+define('WP_CONTENT_URL', '//' . $_SERVER['HTTP_HOST'] . '/wp-content');
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+if (! defined('WP_DEBUG')) {
+    define('WP_DEBUG', false);
+}
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if (! defined('ABSPATH')) {
+    define('ABSPATH', dirname(__FILE__) . '/wp/');
+}
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
